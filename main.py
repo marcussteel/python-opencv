6db42c3371c5f56c162cfde670fcb3cc3ac746d4
marcussteel <resetyourcoin21@gmail.com>
2022-09-30 02:03:59 +0300

    change res and change color to grey

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import numpy as np
 import cv2
-
-print(cv2.__version__)
+from change_resolution import *
+from change_color import change_color_to_grey
 
 cap = cv2.VideoCapture(0)
 
+
+# make_720p(cap) #change resolution.py ile
+
+
 while True:
+    
     # this is reading by frame so it is reading every frame
-    ret, frame = cap.read() 
+    ret, frame = cap.read()
+
+
+    #change resolution.py ile frame i güncelleyebiliriz
+    # frame = rescale_frame(frame, percent=70)
+
+    # change_color_to_grey(cap)
+      
 
     # now we are displayin this fram in our computer
-    cv2.imshow('Frame : ', frame) #imgshow
-
-    #we can put many frames :
-    cv2.imshow('frame1 : ', frame) #imgshow
-
-    #lets change color to greyscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Frame With GRAY : ', gray)  # imgshow
+    cv2.imshow('Frame : ', frame)  # imgshow
 
     #if you see grey bar and it just seemed to be hanging we should write this
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
+#when everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
